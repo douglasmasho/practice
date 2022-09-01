268edc55e77b3eabf9e50b5da2fd4c7aa0450140
Douglas Mashonganyika <51298460+douglasmasho@users.noreply.github.com>
2022-09-01 05:30:57 +0200

    updated cheapestMinibus algorithm and added some console code

--- CabCompany.java
+++ CabCompany.java
@@ -7,13 +7,21 @@ public class CabCompany {
         vehicles = new ArrayList<Vehicle>();
     }
 
+    public String GetAllCars(){
+        String finalString = "";
+        for(int i = 0; i < vehicles.size(); i++){
+            finalString += ("\n" + vehicles.get(i).GetPlateNum());
+        }
+        return finalString;
+    }
+
     public void AddVehicle(Vehicle vehicle){
         vehicles.add(vehicle);
     }
 
     public String CheaperMinibus(double kilometres){
-        //since the first element in the array could be a bus or a sedan, we need to use the smalles possible number instead of the fist vehicle's price
-        double cheapestPrice = Double.NEGATIVE_INFINITY;
+        //since the first element in the array could be a bus or a sedan, we need to use the highest possible number instead of the fist vehicle's price
+        double cheapestPrice = Double.POSITIVE_INFINITY;
         String cheapestPlate = "";
         for(int i = 0; i < vehicles.size(); i++){
             if(vehicles.get(i) instanceof Minibus && vehicles.get(i).GetPrice(kilometres) < cheapestPrice){
--- CompanyProblem.java
+++ CompanyProblem.java
@@ -1,5 +1,15 @@
 public class CompanyProblem {
     public static void main(String[] args){
+        CabCompany company = new CabCompany();
+        Minibus minimbus1 = new Minibus("123", 13, 200);
+        Bus bus1 = new Bus("124", 13, 200);
+        Sedan sedan1 = new Sedan("125", 13, 200);
+
+        company.AddVehicle(minimbus1);
+        company.AddVehicle(bus1);
+        company.AddVehicle(sedan1);
+
+        System.out.println(company.GetAllCars());
 
     }
 }
